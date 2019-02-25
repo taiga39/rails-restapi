341f9d93712a9fac27650148b6f86e848d1415e3
taiga39 <ishikawa.tigers@gmail.com>
2019-02-25 21:56:14 +0900

    modelのテスト（グリーン）
    テスト駆動開発でテストが成功している状態

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :title, presence: true,length:{maximum: 100}
+  validates :body, presence: true,length:{maximum: 500}
+  validates :image, presence: true
+  validates :price, presence: true
 end
