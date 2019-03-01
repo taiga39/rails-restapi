9c81399610fb9bac41c79a47d294e95d12ff569c
taiga39 <ishikawa.tigers@gmail.com>
2019-03-01 17:17:44 +0900

    moduleの記述の削除

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,51 +1,37 @@
-module Api
-  module V1
-    class ProductsController < ApplicationController
-      before_action :set_product, only: [:show, :update, :destroy]
-      protect_from_forgery except: [:create,:update]
-
-      def index
-        @products = Product.all
-        render json: (@products)
-      end
-
-      def create
-        @product = Product.new(product_params)
-        if @product.save
-          render json: (@product),status:201
-        else
-          render json:{error:"404 error"},status:404
-        end
-      end
-
-      def show
-        render json: (@product)
-      end
-
-      def update
-        if @product.update(product_params)
-          render json: (@product),status:200
-        else
-          render json: {error:"404 error"},status:404
-        end
-      end
-
-      def destroy
-        @product.destroy
-        render json: (Product.all),status:200
-
-      end
-
-      private
-
-      def product_params
-        params.permit(:title, :body,:image,:price)
-      end
-
-      def set_product
-        @product = Product.find(params[:id])
-
-      end
+class Api::V1::ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+  protect_from_forgery except: [:create,:update]
+  def index
+    @products = Product.all
+    render json: (@products)
+  end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: (@product),status:201
+    else
+      render json:{error:"404 error"},status:404
     end
+  end
+  def show
+    render json: (@product)
+  end
+  def update
+    if @product.update(product_params)
+      render json: (@product),status:200
+    else
+      render json: {error:"404 error"},status:404
+    end
+  end
+  def destroy
+    @product.destroy
+    render json: (Product.all),status:200
+  end
+  private
+  def product_params
+    params.permit(:title, :body,:image,:price)
+  end
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
