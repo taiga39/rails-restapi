3e25ea308d5be50d8bdfbd40672a7a8ecf568d36
taiga39 <ishikawa.tigers@gmail.com>
2019-03-21 12:22:58 +0900

    RSpecコントローラーのテスト

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :product do
-    title              {"title"}
+    title              {"titletitle"}
     body               {"bodobodybody"}
     image              {"imageurl"}
     price              {1500}
